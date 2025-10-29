c4dd40b8e20ae6637ad312cfd842e92801256b9d
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2025-10-29 21:51:36 +0500

    If else to calculate avg and assign grade

--- Practice_Mid_2025/Program.cs
+++ Practice_Mid_2025/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter Your Name: ");
+string? userName = Console.ReadLine();
+
+Console.WriteLine("Enter marks for the following Subjects: ");
+Console.WriteLine("Maths: ");
+int maths = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Physics: ");
+int physics = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Computer: ");
+int computer = int.Parse(Console.ReadLine());
+
+int total = maths + physics + computer;
+float avg =  total / 3;
+char grade;
+
+if (avg >= 80)
+{
+    grade = 'A';
+}
+else if (avg >= 60)
+{
+    grade = 'B';
+}
+else if (avg >= 40)
+{
+    grade = 'C';
+}
+else
+{
+    grade = 'F';
+}
+
+Console.WriteLine($"Your Avg marks are {avg} and your grade is {grade}.");
