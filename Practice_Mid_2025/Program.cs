f4df50883e6e961f3cb28d8c834a32dcb5b22d0a
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2025-10-29 22:36:13 +0500

    Jagged arrays to store sales data of employees

--- Practice_Mid_2025/Program.cs
+++ Practice_Mid_2025/Program.cs
@@ -1,37 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Enter Your Name: ");
-string? userName = Console.ReadLine();
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Enter Your Name: ");
+//string? userName = Console.ReadLine();
 
-Console.WriteLine("Enter marks for the following Subjects: ");
-Console.WriteLine("Maths: ");
-int maths = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter marks for the following Subjects: ");
+//Console.WriteLine("Maths: ");
+//int maths = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Physics: ");
-int physics = int.Parse(Console.ReadLine());
+//Console.WriteLine("Physics: ");
+//int physics = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Computer: ");
-int computer = int.Parse(Console.ReadLine());
+//Console.WriteLine("Computer: ");
+//int computer = int.Parse(Console.ReadLine());
 
-int total = maths + physics + computer;
-float avg =  total / 3;
-char grade;
+//int total = maths + physics + computer;
+//float avg =  total / 3f;
+//char grade;
 
-if (avg >= 80)
+//if (avg >= 80)
+//{
+//    grade = 'A';
+//}
+//else if (avg >= 60)
+//{
+//    grade = 'B';
+//}
+//else if (avg >= 40)
+//{
+//    grade = 'C';
+//}
+//else
+//{
+//    grade = 'F';
+//}
+
+//Console.WriteLine($"Your Avg marks are {avg} and your grade is {grade}.");
+
+
+//Jagged Arrays Employee Sales Store
+
+Console.Write("Enter Number of employees in your store: ");
+int totalEmployees = int.Parse(Console.ReadLine()!);
+
+double[][] salesData = new double[totalEmployees][];
+
+for (int i = 0; i < totalEmployees; i++)
 {
-    grade = 'A';
-}
-else if (avg >= 60)
-{
-    grade = 'B';
-}
-else if (avg >= 40)
-{
-    grade = 'C';
-}
-else
-{
-    grade = 'F';
+    Console.WriteLine($"Enter Total Number of Sales for {i + 1} employee:");
+    int totalSalesEachEmp = int.Parse(Console.ReadLine()!);
+    salesData[i] = new double[totalSalesEachEmp];
+    for (int j = 0; j < totalSalesEachEmp; j++)
+    {
+        Console.Write($"Employee {i+1} - Sale {j + 1} : ");
+        salesData[i][j] = double.Parse(Console.ReadLine()!);
+    }
 }
 
-Console.WriteLine($"Your Avg marks are {avg} and your grade is {grade}.");
+
+
+double Average(double[] data)
+{
+    double sum = 0;
+    for (int i = 0;i < data.Length; i++)
+    {
+        sum += data[i];
+    }
+    return sum / data.Length;
+}
